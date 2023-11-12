b5df7cb08be1249248d5d83a9949fea559503650
Fabian Vilers <fabian.vilers@dev-one.com>
2023-11-12 09:12:06 +0100

    Add the clear method on debounced function

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,12 +1,20 @@
+export type Debounced<T> = T & {
+  clear(): void;
+};
+
 type F<T extends unknown[]> = (...args: T) => void;
 
-export function debounce<T extends unknown[]>(f: F<T>, timeoutMs = 300): F<T> {
+export function debounce<T extends unknown[]>(
+  f: F<T>,
+  timeoutMs = 300
+): Debounced<F<T>> {
   let timeout: number | undefined = undefined;
 
+  const clear = () => clearTimeout(timeout);
   const wrapper: F<T> = (...args) => {
-    clearTimeout(timeout);
+    clear();
     timeout = setTimeout(() => f(...args), timeoutMs);
   };
 
-  return wrapper;
+  return Object.assign(wrapper, { clear });
 }
